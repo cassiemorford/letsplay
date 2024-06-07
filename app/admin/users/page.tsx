5fb02d791aedf5dd3e065a23b57e8385d5be8324
cassie morford <cassiemorford@gmail.com>
2024-06-07 12:23:11 -0700

    new user link added to user page

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import UserTable from "../../../components/UserTable";
 import prisma from "@/prisma/db";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
 
 const Users = async () => {
   const allUsers = await prisma.user.findMany({
@@ -10,6 +12,14 @@ const Users = async () => {
   });
   return (
     <>
+      <div className="flex justify-end">
+        <Link
+          className={`${buttonVariants({ variant: "default" })} mt-6 mb-6 mr-6`}
+          href="/admin/users/new"
+        >
+          Add New User
+        </Link>
+      </div>
       <UserTable users={allUsers} includeOrganizationColumn />
     </>
   );
